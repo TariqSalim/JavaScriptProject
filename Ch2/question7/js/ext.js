b31f059bb2d627b63900f94b717185decc970251
aarjav89 <aarjav.chandarana@gmail.com>
2019-08-16 20:08:51 -0400

    Completed Ch2 Q7

---
+++ Ch2/question7/js/ext.js
@@ -0,0 +1,49 @@
+// Javascript
+
+function checkfun1()
+{
+ let result=document.getElementById("txt1").value;
+ if (result==14)
+    alert("Correct Answer !!")
+  else {
+    alert("Incorrect Answer.. Try Again")
+  }
+}
+
+function checkfun2()
+{
+  let result=document.getElementById("txt2").value;
+  if (result==24)
+     alert("Correct Answer !!")
+   else {
+     alert("Incorrect Answer.. Try Again")
+   }
+}
+
+function checkfun3()
+{
+  let result=document.getElementById("txt3").value;
+  if (result==11)
+     alert("Correct Answer !!")
+   else {
+     alert("Incorrect Answer.. Try Again")
+   }
+}
+
+function checkfun4(){
+  let result=document.getElementById("txt4").value;
+  if (result==16)
+     alert("Correct Answer !!")
+   else {
+     alert("Incorrect Answer.. Try Again")
+   }
+}
+
+function checkfun5(){
+  let result=document.getElementById("txt5").value;
+  if (result==2)
+     alert("Correct Answer !!")
+   else {
+     alert("Incorrect Answer.. Try Again")
+   }
+}
