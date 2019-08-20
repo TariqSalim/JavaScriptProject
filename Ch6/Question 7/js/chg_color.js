366b2257fc5aee3166eb84c524fb019b2c7a0e03
aarjav89 <aarjav.chandarana@gmail.com>
2019-08-20 19:42:57 -0400

    Completed Ch6 Q7

---
+++ Ch6/Question 7/js/chg_color.js
@@ -0,0 +1,12 @@
+
+function change_bgcolor()
+{
+  let bgColor = document.getElementById("bgcolor").value;
+  document.body.style.backgroundColor = bgColor;
+}
+
+function change_txtcolor()
+{
+  let txtColor = document.getElementById("txtcolor").value;
+  document.getElementById("para").style.color = txtColor;
+}
