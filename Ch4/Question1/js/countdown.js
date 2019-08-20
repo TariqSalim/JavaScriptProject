24800547797012e86aa385b33c203cde29ba0af1
TariqSalim <taurus.tariq@gmail.com>
2019-08-19 21:02:24 -0400

    Completed Ch5 Q6

--- Ch4/Question1/js/countdown.js
+++ Ch4/Question1/js/countdown.js
@@ -1,28 +1,6 @@
 
 
 let sec = 10
-<<<<<<< HEAD
-function blastOff()
-{
-	if(sec>=0){
-	
-	document.write("<img src='Images/rocket.gif'>")
-	
-	document.getElementById("output").innerHTML=sec
-	
-//console.log(sec)
-	
-	}
-	
-	sec--
-	
-	}
-
-function btnclick(){
-
-
-
-=======
 function blastOff(){
 		if(sec>=0){
 			document.getElementById("output").innerHTML=sec
@@ -35,7 +13,6 @@ function blastOff(){
 	}
 
 function btnclick(){
->>>>>>> 8edc5cdb91026ba9957588949c0d9add4a89750f
 	setInterval(function() {
 		blastOff();
 	}, 1000)
---
+++ Ch5/Question6/js/Question6.js
@@ -0,0 +1,35 @@
+// Create a web page that allows th user to toss a con as often as desired. use the Math.random() function to determine the result, where 0 = heads and
+// 1 = tails. If the user's guess matches the computer's toss, the user wins. Use the coin image in the Student Data Files to display the winning result.
+
+
+
+
+
+
+function TossCoin(){
+	
+	let str = prompt("how many times do you want to toss coin")
+	let output = [], input = []
+	
+	for(let i = 0; i <str; i++){
+		output.push(Math.floor((Math.random() * 2)))
+		input[i] = prompt("What's your call, Heads i.e 0 or Tails i.e 1 ?")
+		
+		if (input[i] == output[i]){
+					//document.getElementById("msg").innerHTML="YOU WIN! IT'S HEADS!"
+					document.write("YOU WIN! IT'S HEADS!")
+					//document.getElementById("output").innerHTML="Result of toss :"+"<img src='Images/heads.jpg' height='250' width='200'>"
+					document.write("Result of toss :"+"<img src='Images/heads.jpg' height='250' width='200'><br>")
+	}//close this if
+	else {
+		//document.getElementById("msg").innerHTML="YOU LOSE! IT'S TAILS!"
+		document.write("YOU LOSE! IT'S TAILS!")
+		//document.getElementById("output").innerHTML="Result of toss :"+"<img src='Images/tails.jpg' height='250' width='200'>"
+		document.write("Result of toss :"+"<img src='Images/tails.jpg' height='250' width='200'><br>")
+		}// close else
+	} // close for
+
+
+			console.log(output)
+
+} // close function
